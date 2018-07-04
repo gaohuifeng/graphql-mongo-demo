c6733fc897f1f8da2091e93d5400bb886e8e61d7
ghf <huifeng@teambition.com>
2018-07-04 20:47:09 +0800

    fix

--- src/resolvers/comment.js
+++ src/resolvers/comment.js
@@ -1,22 +1,24 @@
+const db = require('../db')
+
 module.exports = {
   Query: {
-    comment: async (_, params, ctx) => {
+    comment: async (_, params) => {
       console.log('--------', 'query comment by', params.id)
-      const data = await ctx.db.comment.findById(params.id) // .populate({path: 'author'})
+      const data = await db.comment.findById(params.id) // .populate({path: 'author'})
       return data
     },
-    comments: async (_, params, ctx) => {
+    comments: async (_, params) => {
       console.log('--------', 'query comments')
       const pageNo = params.page || 1
       const pageSize = params.size || 10
-      const data = await ctx.db.comment.find({}).skip((pageNo - 1) * pageSize).limit(pageSize)
+      const data = await db.comment.find({}).skip((pageNo - 1) * pageSize).limit(pageSize)
       return data
     }
   },
   Comment: {
-    author: async (obj, params, ctx) => {
-      console.log('--------', 'query user by', obj.author || params.id)
-      const data = await ctx.db.user.findById(obj.author || params.id)
+    author: async (obj, params) => {
+      console.log('--------', 'query user by', obj.author)
+      const data = await db.user.findById(obj.author)
       return data
     }
   }
--- src/resolvers/user.js
+++ src/resolvers/user.js
@@ -1,32 +1,34 @@
+const db = require('../db')
+
 module.exports = {
   Query: {
-    user: async (_, params, ctx) => {
+    user: async (_, params) => {
       console.log('--------', 'query user by', params.id)
-      const data = await ctx.db.user.findById(params.id)
+      const data = await db.user.findById(params.id)
       return data
     },
-    users: async (_, params, ctx) => {
+    users: async (_, params) => {
       console.log('--------', 'query users')
       const pageNo = params.page || 1
       const pageSize = params.size || 10
-      const data = await ctx.db.user.find({}).skip((pageNo - 1) * pageSize).limit(pageSize)
+      const data = await db.user.find({}).skip((pageNo - 1) * pageSize).limit(pageSize)
       return data
     }
   },
 
   Mutation: {
-    createUser: async (_, params, ctx) => {
-      const data = await ctx.db.user.create({name: params.name})
+    createUser: async (_, params) => {
+      const data = await db.user.create({name: params.name})
       return data
     },
 
-    updateUser: async (_, params, ctx) => {
-      await ctx.db.user.update({_id: params.id}, {name: params.name})
+    updateUser: async (_, params) => {
+      await db.user.update({_id: params.id}, {name: params.name})
       return true
     },
 
-    deleteUser: async (_, params, ctx) => {
-      await ctx.db.user.remove({_id: params.id})
+    deleteUser: async (_, params) => {
+      await db.user.remove({_id: params.id})
       return true
     }
   }
